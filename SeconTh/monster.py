026d70e28e2046491bcf9e37e813ae0c8c8b9876
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-10-17 23:32:00 +0900

    몬스터 기본 추가

---
+++ SeconTh/monster.py
@@ -0,0 +1,56 @@
+import random
+
+class Monster:
+    def __init__(self):
+        self.x, self.y = random.randrange(100, 600), random.randrange(100, 600)
+        self.frame=0
+        self.dir=0
+        self.damage=0
+        self.speed=0
+        self.health=0
+        self.time=0
+        self.status=0 #0 : 기본 상태
+       # self.image = load_image('run_animation.png')
+    def render(self):
+        if self.status==0 : #기본
+            pass
+            #self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
+        elif self.status==1 :   #이동
+            pass
+            #self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        elif self.status == 2:  #공격
+            pass
+            #self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+        self.frame += 1
+
+    def search_player(self):
+        pass
+
+    def attack(self):
+        pass
+
+class MonsterT(Monster):
+    def __init__(self):
+        super().__init__()
+        self.monster_type = 1
+        self.health=100
+        #self.image = load_image('run_animation.png')
+class MonsterB(Monster):
+    def __init__(self):
+        super().__init__()
+        self.monster_type = 2
+        self.health = 100
+        #self.image = load_image('run_animation.png')
+class MonsterL(Monster):
+    def __init__(self):
+        super().__init__()
+        self.monster_type = 3
+        self.health = 100
+        #self.image = load_image('run_animation.png')
+class MonsterR(Monster):
+    def __init__(self):
+        super().__init__()
+        self.monster_type = 4
+        self.health = 100
+        #self.image = load_image('run_animation.png')
