5a134ca4fd9547f9a1c259aba5412947dca4d52c
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-10-19 04:16:43 +0900

    게임매니저(게임월드 제작중)

---
+++ SeconTh/gameworld.py
@@ -0,0 +1,115 @@
+from monster import*
+from player import*
+
+
+class GameWorld:
+    def __init__(self):
+
+        self.Check=0
+
+        self.enemies =[]
+        self.enemiesL=[]
+        self.enemiesR = []
+        self.enemiesT = []
+        self.enemiesB = []
+
+        self.penalty=1
+        self.stage=1
+        self.EndStage = 0
+
+        self.playerTime = 30
+        self.playerWhere = 0
+        self.player = Player()
+        self.playerLife=10
+        self.playerWhere=0
+
+    def update(self):
+        self.checkstage()   #1 검사
+                            #2 player update
+                            #3 monster update
+                            #4 gamelogic update
+        pass
+
+    def render(self):
+        self.player.render()
+        if self.playerWhere == 1:
+            self.
+        self.enemies.render()
+        pass
+
+    def enterfield(self):
+        pass
+
+
+    def resetplayer(self):
+        self.player=Player()
+        self.playerWhere = 0
+        self.playerlife-=1
+        self.stage += 1
+        self.playerTime=30
+
+    def resetenemy(self):
+        self.enemiesL = [MonsterL() for i in range(self.stage * 5 * self.penalty)]
+        self.enemiesR = [MonsterR() for i in range(self.stage * 5 * self.penalty)]
+        self.enemiesT = [MonsterT() for i in range(self.stage * 5 * self.penalty)]
+        self.enemiesB = [MonsterB() for i in range(self.stage * 5 * self.penalty)]
+
+        self.enemies = [self.enemiesL, self.enemiesR, self.enemiesT, self.enemiesB]
+
+    def checkstage(self):
+
+        if self.player.hp <= 0 <= self.playerTime:
+            self.penalty +=1
+        elif self.player.hp>=0 and self.checkmonster():
+            pass
+
+    def checkmonster(self):
+        checking_empty=1
+        for enemy in self.enemies:
+            if enemy.health >= 0 :
+                checking_empty=0
+
+        if checking_empty==1:
+            return 1    #아무도 없으면 1 반환
+        else
+            return 0    #존재하면 0 반환
+
+
+    def killplayer(self):
+
+
+
+    def checkplayer(self):
+        if self.playerTime <= 0 or self.player.hp <= 0:
+            self.resetplayer()
+            self.playerWhere=0
+            #2
+        #1  #0  #3
+            #4
+
+        if self.playerWhere==0:
+            if self.player.x <= 0 and 250 <= self.player.y <= 350:
+                self.playerWhere = 1
+            if self.player.x >= 800 and 250 <= self.player.y <= 350:
+                self.playerWhere = 3
+            if self.player.y >= 600 and 350 <= self.player.x <= 450:
+                self.playerWhere = 2
+            if self.player.y <= 0 and 350 <= self.player.x <= 450:
+                self.playerWhere = 4
+        elif self.playerWhere==1:
+            if self.player.x >= 800 and 250 <= self.player.y <= 350:
+                self.playerWhere = 0
+        elif self.playerWhere==2:
+            if self.player.y <= 0 and 250 <= self.player.x <= 350:
+                self.playerWhere = 0
+        elif self.playerWhere==3:
+            if self.player.x <= 0 and 350 <= self.player.y <= 450:
+                self.playerWhere = 0
+        elif self.playerWhere==4:
+            if self.player.y >= 600 and 350 <= self.player.x <= 450:
+                self.playerWhere = 0
+
+
+
+
+
