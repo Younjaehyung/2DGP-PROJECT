d0a5ee62af12c05dc26543107f2f0395e2b0b38c
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-10-18 23:56:25 +0900

    main 추가중

--- SeconTh/main.py
+++ SeconTh/main.py
@@ -1,20 +1,25 @@
 import math
 from pico2d import *
 
+import gameworld
+from gameworld import GameWorld
+
+
 
 def gameobject_update():
-
+    global game
+    game.update()
 
 
 def gameobject_draw():
-
+    global game
+    game.render()
 
 
 def initialize():
+    global game
+    game = GameWorld()
 
-
-
-worldobject
 
 initialize()
 
