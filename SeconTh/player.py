da2a5af69de31e985fe6cba6a8afc99963260ace
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-10-19 15:51:51 +0900

    이동방향 = 스프라이트 방향 일관성 출력 추가

--- SeconTh/player.py
+++ SeconTh/player.py
@@ -12,62 +12,74 @@ class Player:
         self.handle_x = 0
         self.handle_y = 0
 
+        self.flip_x='H'
+        self.flip_y = 0
+
         self.status = 0
 
         self.normal_frame = 0
         self.action_frame=7
         self.image = load_image("resource/Knight/Knight/Knight.png")
+
     def update(self):
         self.attack()
         self.move()
         pass
     def render(self):
-        flip_x ='H'
-        flip_y =0
         if self.handle_x<0:
-            flip_x ='h'
+            self.flip_x ='h'
+        elif self.handle_x>0:
+            self.flip_x = 'H'
         if self.handle_y < 0:
-            flip_y = 0
+            self.flip_y = 0
+        elif self.handle_y > 0:
+            self.flip_y = 0
+
+        if self.status==0:
+            self.action_frame=7
+            self.normal_frame = (self.normal_frame + 1) % 6
+        elif self.status==1:
+            self.action_frame=6
+            self.normal_frame = (self.normal_frame + 1) % 8
 
 
 
-
-
-        self.normal_frame = (self.normal_frame +1)%6
 
         self.image.clip_composite_draw(
             self.normal_frame * 100,  # 이미지의 왼쪽 상단 x좌표
             self.action_frame * 100,  # 이미지의 왼쪽 상단 y좌표
             100,
             100,
-            flip_y,
-            flip_x,
+            self.flip_y,
+            self.flip_x,
             self.x,
             self.y,
             250,
             250)
         pass
 
+
     def attack(self):
         pass
 
     def move(self):
-        if self.status != 0:
-            return
+
         events = get_events()
         for event in events:
-            if self.status==0:
+            #if self.status==0:
                 if event.type == SDL_KEYDOWN:
                     if event.key == SDLK_UP:
                         self.handle_y =self.handle_y+ 1
-
+                        self.status = 1
                     if event.key == SDLK_LEFT:
                         self.handle_x -= 1
+                        self.status = 1
                     if event.key == SDLK_RIGHT:
                         self.handle_x += 1
-
+                        self.status = 1
                     if event.key == SDLK_DOWN:
                         self.handle_y -= 1
+                        self.status = 1
                 elif event.type == SDL_KEYUP:
                     if event.key == SDLK_UP:
                         self.handle_y -= 1
@@ -77,6 +89,9 @@ class Player:
                         self.handle_x -= 1
                     if event.key == SDLK_LEFT:
                         self.handle_x += 1
+                    self.status = 0
+
+
 
 
 
