fd912a0e9d9e51edad8cfc9474f951eaa52a6e32
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-10-19 15:30:53 +0900

    플레이어 이동기 구현

--- SeconTh/player.py
+++ SeconTh/player.py
@@ -1,5 +1,7 @@
 import random
 
+from pico2d import *
+
 class Player:
     def __init__(self):
         self.x ,self.y=400,300
@@ -7,16 +9,56 @@ class Player:
         self.hp=random.randint(40,100)
         self.speed=random.randint(10,30)
 
-    def update(self):
+        self.handle_x = 0
+        self.handle_y = 0
 
+        self.status = 0
+
+        self.normal_frame = 0
+        self.action_frame=7
+        self.image = load_image("resource/Knight/Knight/Knight.png")
+    def update(self):
+        self.attack()
+        self.move()
+        pass
     def render(self):
 
+        pass
 
     def attack(self):
+        pass
 
     def move(self):
+        if self.status != 0:
+            return
+        events = get_events()
+        for event in events:
+            if self.status==0:
+                if event.type == SDL_KEYDOWN:
+                    if event.key == SDLK_UP:
+                        self.handle_y =self.handle_y+ 1
+
+                    if event.key == SDLK_LEFT:
+                        self.handle_x -= 1
+                    if event.key == SDLK_RIGHT:
+                        self.handle_x += 1
+
+                    if event.key == SDLK_DOWN:
+                        self.handle_y -= 1
+                elif event.type == SDL_KEYUP:
+                    if event.key == SDLK_UP:
+                        self.handle_y -= 1
+                    if event.key == SDLK_DOWN:
+                        self.handle_y += 1
+                    if event.key == SDLK_RIGHT:
+                        self.handle_x -= 1
+                    if event.key == SDLK_LEFT:
+                        self.handle_x += 1
 
 
+
+        self.x += (self.speed/50) * self.handle_x
+        self.y += (self.speed/50) * self.handle_y
 
     def reset(self):
         self.hp=0
