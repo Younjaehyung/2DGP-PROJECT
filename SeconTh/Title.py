7ac708f5e9a32d424a1299da08d3cb404bed59ea
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-11-26 15:32:28 +0900

    배경 움직임 추가

--- SeconTh/Title.py
+++ SeconTh/Title.py
@@ -8,20 +8,45 @@ import collider
 
 class Title:
     def __init__(self):
-       self.image1 = load_image("resource/title/title1.png")
-       self.image2 = load_image("resource/title/title2.png")
-       self.image3 = load_image("resource/title/title3.png")
-       self.image4 = load_image("resource/title/title4.png")
+        self.image1 = load_image("resource/title/title1.png")
+        self.image2 = load_image("resource/title/title2.png")
+        self.imagen2 = load_image("resource/title/title2.png")
+        self.image3 = load_image("resource/title/title3.png")
+        self.imagen3 = load_image("resource/title/title3.png")
+        self.image4 = load_image("resource/title/title4.png")
+        self.title2_x = 600
+        self.title2_nx = -600
+        self.title3_x = 600
+        self.title3_nx = -600
 
     def update(self):
-        pass
+        self.title2_x += 3
+        self.title2_nx += 3
+        self.title3_x += 1
+        self.title3_nx += 1
+
+        if self.title2_x > 1800:
+            self.title2_x = 600
+
+        if self.title2_nx > 600:
+            self.title2_nx = -600
+
+        if self.title3_x > 1800:
+            self.title3_x = 600
+
+        if self.title3_nx > 600:
+            self.title3_nx = -600
+
 
     def draw(self):
-        self.image1.clip_draw(0, 0, 1200, 800, 600,400)
-        self.image2.clip_draw(0, 0, 1200, 800, 600,400)
-        self.image3.clip_draw(0, 0, 1200, 800, 600,400)
-        self.image4.clip_draw(0, 0, 1200, 800, 600,400)
+        self.image1.clip_draw(0, 0, 1200, 800, 600, 400)
+        self.imagen2.clip_draw(0, 0, 1200, 800, self.title2_nx, 400)
+        self.image2.clip_draw(0, 0, 1200, 800, self.title2_x, 400)
+        self.image3.clip_draw(0, 0, 1200, 800, self.title3_nx, 400)
+        self.image3.clip_draw(0, 0, 1200, 800, self.title3_x, 400)
+        self.image4.clip_draw(0, 0, 1200, 800, 600, 400)
         pass
+
 
 class End:
     def __init__(self):
