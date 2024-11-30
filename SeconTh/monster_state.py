02dea4afab66e4c74e8091396a6e2ff85062c724
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-11-30 15:14:04 +0900

    적 사망 시 위에 아이콘 추가, 죽는 이펙트 추가

--- SeconTh/monster_state.py
+++ SeconTh/monster_state.py
@@ -6,8 +6,7 @@ from sdl2 import *
 
 class Idle:
     @staticmethod
-    def enter(player,e):
-
+    def enter(player, e):
         player.handle_x = 0
         player.handle_y = 0
         player.normal_frame = 0
@@ -15,14 +14,11 @@ class Idle:
         pass
 
     @staticmethod
-    def exit(player,e):
-
+    def exit(player, e):
         pass
 
     @staticmethod
     def draw(player):
-
-
         player.image.clip_composite_draw(
             player.normal_frame * 100,  # 이미지의 왼쪽 상단 x좌표
             player.action_frame * 100,  # 이미지의 왼쪽 상단 y좌표
@@ -35,15 +31,13 @@ class Idle:
             300,
             300)
 
-
         pass
 
     @staticmethod
     def do(player):
-
-        if get_time()-player.idle_time > 0.15:
+        if get_time() - player.idle_time > 0.15:
             player.normal_frame = (player.normal_frame + 1) % 6
-            player.idle_time =get_time()
+            player.idle_time = get_time()
 
         # if player.search_player():
         #     player.state_machine.add_event(('SEARCH', 0))
@@ -51,14 +45,13 @@ class Idle:
         pass
 
 
-
 class Run:
     @staticmethod
-    def enter(player,e):
+    def enter(player, e):
 
         if Search_event(e):
             player.Attack_status = 0
-            player.action_frame =2
+            player.action_frame = 2
 
         player.action_frame = 4
         player.normal_frame = 0
@@ -66,7 +59,7 @@ class Run:
         pass
 
     @staticmethod
-    def exit(player,e):
+    def exit(player, e):
 
         pass
 
@@ -87,10 +80,9 @@ class Run:
 
     @staticmethod
     def do(player):
-        if get_time()-player.run_time > 0.2:
+        if get_time() - player.run_time > 0.2:
             player.normal_frame = (player.normal_frame + 1) % 8
-            player.run_time =get_time()
-
+            player.run_time = get_time()
 
             # 플레이어를 추적하는 로직 추가
 
@@ -110,9 +102,9 @@ class Run:
             player.handle_x /= distance
             player.handle_y /= distance
 
-        if player.handle_x >0:
+        if player.handle_x > 0:
             player.flip_x = 'H'
-            player.dir =1
+            player.dir = 1
 
         else:
             player.flip_x = 'h'
@@ -123,20 +115,20 @@ class Run:
 
         pass
 
+
 class Attack:
     @staticmethod
-    def enter(player,e):
+    def enter(player, e):
 
         player.Attack_status = 1
-        player.action_frame =3
-
+        player.action_frame = 3
 
         player.normal_frame = 0
 
         pass
 
     @staticmethod
-    def exit(player,e):
+    def exit(player, e):
         player.Attack_status = 0
 
         pass
@@ -158,25 +150,31 @@ class Attack:
 
     @staticmethod
     def do(player):
-        if get_time()-player.attack_time > 0.2:
+        if get_time() - player.attack_time > 0.2:
             player.normal_frame = (player.normal_frame + 1) % 6
-            player.attack_time =get_time()
+            player.attack_time = get_time()
+
+        if player.health <= 0:
+            player.state_machine.add_event(('DEAD', 0))
+            return  # 상태 전환 후 더 이상 실행하지 않음
 
             # 플레이어를 추적하는 로직 추가
         if player.normal_frame == 0:
-
             player.state_machine.add_event(('TIME_OUT', 0))
 
         pass
+
 
 class Dead:
     @staticmethod
     def enter(player, e):
         player.normal_frame = 0
         player.action_frame = 1
-        player.dead_time =get_time()
+        player.dead_time = get_time()
         player.fly_back_distance = 50  # 뒤로 날아가는 최대 거리
         player.fly_back_speed = 3  # 뒤로 날아가는 속도
+        player.sfx_frame = 0  # SFX 이미지의 초기 프레임
+        player.sfx_time = get_time()  # SFX 애니메이션 시간 초기화
         pass
 
     @staticmethod
@@ -196,17 +194,32 @@ class Dead:
             player.y,
             300,
             300)
-        pass
+
+        player.icon_image.clip_draw(
+            0, 0, 32, 32,  # 아이콘 크기
+            player.x, player.y + 50,  # 몬스터 위쪽에 렌더링
+            50, 50  # 출력 크기
+        )
+
+        player.sfx_image.clip_draw(
+            player.sfx_frame * 100, 0,  # SFX 프레임의 x 좌표
+            100, 100,  # SFX 이미지의 너비와 높이
+            player.x, player.y,  # 몬스터 위쪽에 렌더링
+            75, 75  # 출력 크기
+        )
 
     @staticmethod
     def do(player):
-        if get_time()-player.dead_time > 0.1:
+        if get_time() - player.dead_time > 0.1:
             player.normal_frame = player.normal_frame + 1
-            player.dead_time =get_time()
+            player.dead_time = get_time()
 
         if player.normal_frame == 5:
             player.state_machine.add_event(('TIME_OUT', 0))
 
+        if get_time() - player.sfx_time > 0.1 and player.sfx_frame < 6:  # 마지막 프레임까지만 업데이트
+            player.sfx_frame += 1
+            player.sfx_time = get_time()
 
         if player.fly_back_distance > 0:
             fly_x = player.dir * -1 * player.fly_back_speed  # 반대 방향으로 이동
@@ -244,5 +257,4 @@ class Death:
 
     @staticmethod
     def do(player):
-
         pass
