61285cebaf677d99bc6a310fbc7a53193610d546
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-12-07 01:45:15 +0900

    플레이어 전체 시간 조정

--- SeconTh/player_state.py
+++ SeconTh/player_state.py
@@ -6,7 +6,7 @@ from state_machine import *
 class Idle:
 
     @staticmethod
-    def enter(player,e):
+    def enter(player, e):
         print('idle')
 
         if z_down(e):
@@ -15,8 +15,7 @@ class Idle:
 
         else:
             player.attack_status = 0
-            player.action_frame=7
-
+            player.action_frame = 7
 
         player.handle_x = 0
         player.handle_y = 0
@@ -24,7 +23,7 @@ class Idle:
         pass
 
     @staticmethod
-    def exit(player,e):
+    def exit(player, e):
 
         pass
 
@@ -47,35 +46,33 @@ class Idle:
     def do(player):
 
         player.normal_frame = (player.normal_frame + 1) % 6
-        if player.action_frame== 5 and player.normal_frame == 0:
+        if player.action_frame == 5 and player.normal_frame == 0:
             player.state_machine.add_event(('TIME_OUT', 0))
             return
 
         pass
 
 
-
 class Run:
     @staticmethod
-    def enter(player,e):
+    def enter(player, e):
 
         if z_down(e):
             player.attack_status = 1
-            player.action_frame=3
+            player.action_frame = 3
 
         else:
             player.attack_status = 0
             player.action_frame = 6
 
-
-
         if right_down(e) or left_up(e):  # 오른쪽으로 RUN
             player.handle_x += 1
             if right_down(e):
                 print("right_down")
-            else : print ("left_up")
+            else:
+                print("left_up")
 
-        if left_down(e) or right_up(e) :  # 왼쪽으로 RUN
+        if left_down(e) or right_up(e):  # 왼쪽으로 RUN
             player.handle_x -= 1
             if left_down(e):
                 print("left_down")
@@ -93,7 +90,8 @@ class Run:
             player.handle_y -= 1
             if down_down(e):
                 print("down_down")
-            else : print ("up_up")
+            else:
+                print("up_up")
 
         if down_down(e):
             player.keydown[3] = 1
@@ -119,78 +117,6 @@ class Run:
         pass
 
     @staticmethod
-    def exit(player,e):
-
-        pass
-
-    @staticmethod
-    def draw(player):
-        player.image.clip_composite_draw(
-            player.normal_frame * 100,  # 이미지의 왼쪽 상단 x좌표
-            player.action_frame * 100,  # 이미지의 왼쪽 상단 y좌표
-            100,
-            100,
-            player.flip_y,
-            player.flip_x,
-            player.x,
-            player.y,
-            250,
-            250)
-        pass
-
-    @staticmethod
-    def do(player):
-        speed=1
-
-
-
-        if player.action_frame == 3:
-            player.normal_frame = (player.normal_frame + 1) % 8
-            if player.job ==1:
-                speed = 1
-            if player.job ==2:
-                speed = 2
-            if player.job ==3:
-                speed = 3
-            if player.job ==4:
-                speed = 1
-            if player.normal_frame == 0:
-                player.state_machine.add_event(('TIME_OUT', 0))
-                return
-        else :
-            player.normal_frame = (player.normal_frame + 1) % 8
-        player2_x = player.x
-        player2_y = player.y
-        player2_x += (player.speed) * (get_time() - player.run_time) * player.handle_x*speed
-        player2_y += (player.speed) * (get_time() - player.run_time) * player.handle_y*speed
-
-        if player2_x >800 or player2_x <0:
-            pass
-        else:
-
-            player.x += (player.speed) * (get_time() - player.run_time) * player.handle_x*speed
-            player.y += (player.speed) * (get_time() - player.run_time) * player.handle_y*speed
-
-        player.run_time = get_time()
-
-        if not player.keydown[0] and not player.keydown[1] and not player.keydown[2] and not player.keydown[3]  :
-            player.state_machine.add_event(('Idle', 0))
-
-
-        pass
-
-
-class Hit:
-
-    @staticmethod
-    def enter(player, e):
-        player.normal_frame = 0
-
-        player.action_frame = 2
-
-        pass
-
-    @staticmethod
     def exit(player, e):
 
         pass
@@ -208,7 +134,74 @@ class Hit:
             player.y,
             250,
             250)
+        pass
 
+    @staticmethod
+    def do(player):
+        speed = 1
+
+        if player.action_frame == 3:
+            player.normal_frame = (player.normal_frame + 1) % 8
+            if player.job == 1:
+                speed = 1
+            if player.job == 2:
+                speed = 2
+            if player.job == 3:
+                speed = 3
+            if player.job == 4:
+                speed = 1
+            if player.normal_frame == 0:
+                player.state_machine.add_event(('TIME_OUT', 0))
+                return
+        else:
+            player.normal_frame = (player.normal_frame + 1) % 8
+        player2_x = player.x
+        player2_y = player.y
+        player2_x += (player.speed) * (get_time() - player.run_time) * player.handle_x * speed
+        player2_y += (player.speed) * (get_time() - player.run_time) * player.handle_y * speed
+
+        if player2_x > 800 or player2_x < 0:
+            pass
+        else:
+
+            player.x += (player.speed) * (get_time() - player.run_time) * player.handle_x * speed
+            player.y += (player.speed) * (get_time() - player.run_time) * player.handle_y * speed
+
+        player.run_time = get_time()
+
+        if not player.keydown[0] and not player.keydown[1] and not player.keydown[2] and not player.keydown[3]:
+            player.state_machine.add_event(('Idle', 0))
+
+        pass
+
+
+class Hit:
+
+    @staticmethod
+    def enter(player, e):
+        player.normal_frame = 0
+
+        player.action_frame = 2
+
+        pass
+
+    @staticmethod
+    def exit(player, e):
+        pass
+
+    @staticmethod
+    def draw(player):
+        player.image.clip_composite_draw(
+            player.normal_frame * 100,  # 이미지의 왼쪽 상단 x좌표
+            player.action_frame * 100,  # 이미지의 왼쪽 상단 y좌표
+            100,
+            100,
+            player.flip_y,
+            player.flip_x,
+            player.x,
+            player.y,
+            250,
+            250)
 
         pass
 
@@ -216,8 +209,7 @@ class Hit:
     def do(player):
         player.normal_frame = (player.normal_frame + 1) % 4
 
-
-        if player.normal_frame ==0:
+        if player.normal_frame == 0:
             player.state_machine.add_event(('TIME_OUT', 0))
 
         pass
@@ -236,7 +228,6 @@ class Spawn:
 
     @staticmethod
     def exit(player, e):
-
         pass
 
     @staticmethod
@@ -273,7 +264,6 @@ class Spawn:
             500,
             500)
 
-
         pass
 
     @staticmethod
@@ -287,12 +277,13 @@ class Spawn:
 
         pass
 
+
 class Dead:
     @staticmethod
     def enter(player, e):
         player.normal_frame = 0
         player.action_frame = 1
-        player.dead_time =get_time()
+        player.dead_time = get_time()
         pass
 
     @staticmethod
@@ -316,9 +307,9 @@ class Dead:
 
     @staticmethod
     def do(player):
-        if get_time()-player.dead_time > 0.5:
+        if get_time() - player.dead_time > 0.5:
             player.normal_frame = (player.normal_frame + 1) % 4
-            player.dead_time =get_time()
+            player.dead_time = get_time()
 
         if player.normal_frame == 3:
             player.state_machine.add_event(('TIME_OUT', 0))
@@ -354,5 +345,4 @@ class Death:
 
     @staticmethod
     def do(player):
-
         pass
